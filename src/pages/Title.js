f35b37b08209575c4f379f678a03d82f240baaa1
akhzan <rizal@payfazz.com>
2020-07-13 11:28:02 +0700

    add responsiveness

--- src/pages/Title.js
+++ src/pages/Title.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-closing-bracket-location */
 /* eslint-disable react/jsx-no-target-blank */
 import React from 'react'
 import dayjs from 'dayjs'
@@ -5,7 +6,7 @@ import dayjs from 'dayjs'
 const Title = ({ date }) => {
   return (
     <div className="app-title">
-      <div className="app-title__title --bold">Covid 19 - Indonesia</div>
+      <h1 className="app-title__title --bold">Covid 19 - Indonesia</h1>
       <div className="app-title__description">
         Last updated data: {dayjs(date).format('DD/MM/YYYY')}
       </div>
@@ -13,7 +14,10 @@ const Title = ({ date }) => {
         The purpose of this page is for experimental research only. If
         you&apos;re looking for data accuracy please refer to{' '}
         <span>
-          <a target="_blank" href="https://covid19.go.id/">
+          <a
+            rel="noopener noreferrer"
+            target="_blank"
+            href="https://covid19.go.id/">
             Covid19.go.id
           </a>
         </span>
